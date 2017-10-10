3ab70acd0b44cf1995b1db3b437ded687ab8a939
kotomisak <jenicek.michal@gmail.com>
2017-10-10 17:25:30 +0200

    Add basic binding.

--- app/src/main/java/com/scodash/ui/MainActivity.kt
+++ app/src/main/java/com/scodash/ui/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.scodash.ui
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.scodash.R
+import com.scodash.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainView {
 
+	private lateinit var viewModel: MainViewModel
+	private lateinit var viewDataBinding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+		viewDataBinding = ActivityMainBinding.inflate(layoutInflater)
+		viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+		viewDataBinding.viewModel = viewModel
+		viewDataBinding.executePendingBindings()
+
 	}
 
 
